3c3123a52c99989bbca379d339b0632162927770
aayubxon701 <aayubxon701@gmail.com>
2023-09-21 19:20:27 +0900

    web server start

---
+++ server.js
@@ -0,0 +1,30 @@
+console.log("Hello world");
+const express = require("express");
+const app = express();
+const http = require("http");
+
+// 1 Kirish codlar
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// 2 Section code
+
+// 3 Views code
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+// 4 Routing code
+app.get("/hello", (req, res) => {
+  res.end("<h1>HEllO WORLD BY LEO</h1>");
+});
+
+app.get("/gift", (req, res) => {
+   res.end("<h1>siz sovgalar bolimidasiz</h1>");
+ });
+
+const server = http.createServer(app);
+let PORT = 3000;
+server.listen(PORT, function () {
+  console.log(`The server started,${PORT}`);
+});
