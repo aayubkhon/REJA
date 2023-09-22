b838a26817607b10e02e2352855dc7cf7db2725d
aayubxon701 <aayubxon701@gmail.com>
2023-09-22 23:44:49 +0900

    feat:update

--- server.js
+++ server.js
@@ -1,67 +1,33 @@
-// console.log("Hello world");
-// const express = require("express");
-// const app = express();
-// const http = require("http");
-
-// // 1 Kirish codlar
-// app.use(express.static("public"));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-
-// // 2 Section code
-
-// // 3 Views code
-// app.set("views", "views");
-// app.set("view engine", "ejs");
-
-// // 4 Routing code
-// app.get("/hello", (req, res) => {
-//   res.end("<h1>HEllO WORLD BY LEO</h1>");
-// });
-
-// app.get("/gift", (req, res) => {
-//    res.end("<h1>siz sovgalar bolimidasiz</h1>");
-//  });
-
-// const server = http.createServer(app);
-// let PORT = 3000;
-// server.listen(PORT, function () {
-//   console.log(`The server started,${PORT}`);
-// });
-
-
-
-
-
-
-
 console.log("Hello world");
 const express = require("express");
 const app = express();
 const http = require("http");
 
-// 1 Kirish codlar
+// 1 kirish codelar
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// 2 Section code
-
+// Sesseion code
 // 3 Views code
 app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4 Routing code
-app.get("/hello", (req, res) => {
-  res.end("<h1>HEllO WORLDddddd BY LEO</h1>");
-});
+// Post malumotni ozi bilan birga olip keladi va Date base ga yozadi
+app.post("/create-item",(req,res)=>{
+  console.log(req.body);
+  res.json({test: "Hello world"})
 
-app.get("/gift", (req, res) => {
-   res.end("<h1>siz sovgalar bolimidasiz</h1>");
- });
+})
+
+// Date base dan malumot olish uchun get ishlatilinadi
+app.get("/",function(req,res){
+  res.render('harid')
+})
 
 const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function () {
-  console.log(`The server started,${PORT}`);
+  console.log(`The server on ${PORT}`);
 });
