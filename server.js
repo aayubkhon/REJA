e0b660949e3cea34fa784ab864ca150aa0b2d58d
aayubxon701 <aayubxon701@gmail.com>
2023-09-26 17:30:28 +0900

    feat:update author

--- server.js
+++ server.js
@@ -2,29 +2,39 @@ console.log("Hello world");
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data)
+  }
+});
 
 // 1 kirish codelar
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Sesseion code
+// 2 Sesseion code
 // 3 Views code
 app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4 Routing code
 // Post malumotni ozi bilan birga olip keladi va Date base ga yozadi
-app.post("/create-item",(req,res)=>{
-  console.log(req.body);
-  res.json({test: "Hello world"})
+app.post("/create-item", (req, res) => {});
 
-})
+app.get("/author", (req, res) => {
+  res.render("author", { user: user });
+});
 
 // Date base dan malumot olish uchun get ishlatilinadi
-app.get("/",function(req,res){
-  res.render('harid')
-})
+app.get("/", function (req, res) {
+  res.render("harid");
+});
 
 const server = http.createServer(app);
 let PORT = 3000;
