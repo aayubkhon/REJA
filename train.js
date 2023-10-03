8cbe7212b665c9589eb44ca91dbeaa0b5fac8fc0
aayubxon701 <aayubxon701@gmail.com>
2023-10-03 20:10:32 +0900

    feat:CRUD

--- train.js
+++ train.js
@@ -1,9 +1,12 @@
-// 📌  E-TASK: E-Task: Shunday function tuzing, u bitta string argumentni qabul qilib osh stringni teskari qilib return qilsin
 
-// masalan: getReverse("hello") return qilsin "olleh"
+// 📌  F-Task: findDoublers function tuzing,
+// unga faqat bitta string argument pass bolib, agar
+//  stringda bir hil harf qatnashgan bolsa true,
+//   qatnashmasa false qaytarishi kerak.
+// masalan: getReverse("hello") return true return qiladi
 
-// function getReverse(smt) {
-//   return smt.split("").reverse().join("");
-// }
-// console.log(getReverse("hello"));
+function finDoublers(smt) {
+  return smt.split("").map((e,index,arr) => (arr.includes(e,index +1))).includes(true);
+}
 
+console.log(finDoublers("hello"));
